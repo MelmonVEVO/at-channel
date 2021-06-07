d2315a721bb4a44f88f906d407d75adbaab38745
Melmon <dylan.drescher@gmail.com>
2021-06-07 21:35:23 +0100

    ..

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='atchannel',
+    name='@channel',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
